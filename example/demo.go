d81bba7398fffd4b50fccfb85092b14212313633
秦新 <qinxin_0720@live.com>
2018-12-01 11:52:15 +0800

    demo fixes issues #3

--- example/demo.go
+++ example/demo.go
@@ -16,7 +16,7 @@ func main() {
     if err != nil {
         return
     }
-    statusCode, _, _ := sc.SendSMS(SmsClient.Params{"1500000000", "阿里云短信", "SMS_000000", `{"code":"12345“}`})
+    statusCode, _, _ := sc.SendSMS(SmsClient.Params{"1500000000", "阿里云短信", "SMS_000000", `{"code":"12345"}`})
     if statusCode == http.StatusOK {
         log.Println("发送成功")
     } else {
